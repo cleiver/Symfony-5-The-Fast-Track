7b9e1fec1dacc784293d7b4b0aca6b5de5e7ad53
Cleiver Carvalhal Carneiro <cleiver@gmail.com>
2020-06-11 17:41:12 -0300

    more caching

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -45,6 +45,20 @@ class ConferenceController extends AbstractController
     }
 
     /**
+     * @Route("/conference_header", name="conference_header")
+     */
+    public function conferenceHeader(ConferenceRepository $conferenceRepository)
+    {
+        $response = new Response($this->twig->render('conference/header.html.twig', [
+            'conferences' => $conferenceRepository->findAll(),
+        ]));
+
+        $response->setSharedMaxAge(3600);
+
+        return $response;
+    }
+
+    /**
      * @Route("/conference/{slug}", name="conference")
      */
     public function show(Request $request, Conference $conference, ConferenceRepository $conferenceRepository, CommentRepository $commentRepository, string $photoDir)
